e96410afe669a8fa5a59e0cb9b2d90e9ecd15903
Norbert Songin <wezyrnorbert@gmail.com>
2015-11-09 20:40:40 +0100

    Moved Video class to separate file. Cleaned project a little.

--- server.js
+++ server.js
@@ -9,87 +9,14 @@ path = require('path'),
 url = require('url'),
 events = require('events'),
 async = require('async'),
-YoutubeMp3Downloader = require('youtube-mp3-downloader'),
-ytdl = require('ytdl-core');
+ytdl = require('ytdl-core'),
+Video = require('./src/models/video.js');
 
 const ytApiKey = "AIzaSyBhtvRxAa5jePf6x0BRQCK-BBZ5-mhGwss";
 const ytApiUrl = "https://www.googleapis.com/youtube/v3";
-const serverIp = "0.0.0.0";
 const serverPort = process.env.PORT || 8080;
-const mp3DirSpaceLimit = 75 * 1000 * 1000;
-const autoPingInterval = 15 * 60 * 1000;
-
-function Video (videoId) {
-    this.id = videoId;
-    this.size = 0;
-    this.isDownloaded = false;
-    this.onDownloadCallbacks = [];
-    this.yt2mp3;
-};
-Video.prototype.getInfo = function (callback) {
-    var video = this;
-    var requestUrl = ytApiUrl + "/videos?part=snippet" 
-                     + "&id=" + video.id 
-                     + "&fields=items(snippet(title))" 
-                     + "&key=" + ytApiKey;
-    https.get(requestUrl, function (searchResponse) {
-        var data = "";
-        searchResponse.on('data', function (dataChunk) {
-            data += dataChunk;
-        });
-        searchResponse.on('end', function () {
-            var videoInfo = JSON.parse(data);
-            callback(null, {
-                title: videoInfo.items[0].snippet.title
-            });
-        });
-    }).on('error', function (err) {
-        callback(err, null);
-    });
-};
-Video.prototype.download = function () {
-    var video = this;
-    //Configure YoutubeMp3Downloader.
-    if (typeof video.yt2mp3 == 'undefined') {
-        video.yt2mp3 = new YoutubeMp3Downloader({
-            "ffmpegPath": __dirname + "/ffmpeg/bin/" + (os.platform() == 'win32' ? "ffmpeg.exe" : "ffmpeg"),             // Where is the FFmpeg binary located? 
-            "outputPath": __dirname + "/mp3",                               // Where should the downloaded and encoded files be stored? 
-            "youtubeVideoQuality": "highest",                               // What video quality should be used? 
-            "queueParallelism": 4,                                         // How many parallel downloads/encodes should be started? 
-            "progressTimeout": 4000                                         // How long should be the interval of the progress reports 
-        });
-    }
-    if (typeof video.onDownloadCallbacks == 'undefined') {
-        video.onDownloadCallbacks = [];
-    }
-    this.getInfo(function (err, videoInfo) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            video.yt2mp3.download(video.id, video.id + "/" + videoInfo.title + ".mp3");
-            console.log('Downloading ' + video.id + '...');
-            video.yt2mp3.on('progress', function (progress) {
-                console.log('Progress: ' + progress.progress.percentage + '%');
-            });
-            video.yt2mp3.on('error', function (err) {
-                console.log(err);
-                for (var i = 0; i < video.onDownloadCallbacks.length; i++) {
-                    this.onDownloadCallbacks[i](err, null);
-                }
-            });
-            video.yt2mp3.on('finished', function (data) {
-                video.isDownloaded = true;
-                fs.stat(data.file, function (err, stats) {
-                    video.size = stats.size;
-                });
-                for (var i = 0; i < video.onDownloadCallbacks.length; i++) {
-                    video.onDownloadCallbacks[i](null, data);
-                }
-            });
-        }
-    });
-};
+const mp3DirSpaceLimit = 75 * 1024 * 1024;  // 75Mb
+const autoPingInterval = 15 * 60 * 1000;    // 15 minutes
 
 var videos = [];
 
@@ -112,7 +39,7 @@ removeDir(__dirname + "/mp3", function (err) {
 var startTime = Date.now();
 function upTime() {
     return (Date.now() - startTime);
-};
+}
 
 // Auto ping app.
 setInterval(function () {
@@ -149,7 +76,7 @@ app.get('/search', function(request, response) {
             + "&type=video" 
             + "&maxResults=3" 
             + "&order=relevance" 
-            + "&key=" + ytApiKey
+            + "&key=" + ytApiKey;
         https.get(requestUrl, function (searchResponse) {
             var data = "";
             searchResponse.on('data', function (dataChunk) {
@@ -277,24 +204,26 @@ app.get('/convert', function(request, response) {
             // Still downloading, add onDownload callback.
                 else {
                     video.onDownloadCallbacks.push(function (err, data) {
-                        //console.log(files[0]);
+                        if (err) {
+                            console.log("Error downloading: " + err);
+                            return;
+                        }
                         var fileName = files[0];
                         var filePath = fileDir + "/" + fileName;
                         fs.stat(filePath, function (err, fileStats) {
                             if (err) {
                                 console.log("Error reading '" + filePath + "': " + err);
+                                return;
                             }
-                            else {
-                                var fileSize = fileStats.size;
-								response.set('Access-Control-Allow-Origin', "*");	
-                                response.set('Content-Type', 'application/json');
-                                var mp3Info = {
-                                    fileName: fileName,
-                                    fileSize: fileSize,
-                                    downloadLink: '/download?v=' + videoId
-                                };
-                                response.status(200).send(JSON.stringify(mp3Info));
-                            }
+                            var fileSize = fileStats.size;
+                            response.set('Access-Control-Allow-Origin', "*");
+                            response.set('Content-Type', 'application/json');
+                            var mp3Info = {
+                                fileName: fileName,
+                                fileSize: fileSize,
+                                downloadLink: '/download?v=' + videoId
+                            };
+                            response.status(200).send(JSON.stringify(mp3Info));
                         });
                     });
                 }
@@ -324,22 +253,18 @@ app.get('/download', function(request, response) {
                 var filePath = fileDir + "/" + fileName;
                 fs.stat(filePath, function (error, fileStats) {
                     if (err) {
-                            console.log("Error reading '" + filePath + "': " + err);
+                        console.log("Error reading '" + filePath + "': " + err);
+                        return;
                     }
-                    else {
-                        var fileSize = fileStats.size;
-                        var filePath = fileDir + "/" + fileName;
-                        response.set('Content-Type', 'audio/mpeg');
-                        response.set('Content-Length', fileSize.toString());
-                        response.set('Content-Disposition', 'attachment; filename="' + fileName + '"');
-						response.set('Access-Control-Allow-Origin', "*");
-                        response.status(200);
-                        var readStream = fs.createReadStream(filePath);
-                        readStream.pipe(response);
-                        readStream.on("close", function () {
-
-                        });
-                    }
+                    var fileSize = fileStats.size;
+                    var filePath = fileDir + "/" + fileName;
+                    response.set('Content-Type', 'audio/mpeg');
+                    response.set('Content-Length', fileSize.toString());
+                    response.set('Content-Disposition', 'attachment; filename="' + fileName + '"');
+                    response.set('Access-Control-Allow-Origin', "*");
+                    response.status(200);
+                    var readStream = fs.createReadStream(filePath);
+                    readStream.pipe(response);
                 });
             }
         });
@@ -361,7 +286,7 @@ function removeDir(location, next) {
     }
     fs.readdir(location, function (err, files) {
         async.each(files, function (file, cb) {
-            file = location + '/' + file
+            file = location + '/' + file;
             fs.stat(file, function (err, stat) {
                 if (err) {
                     return cb(err);
@@ -378,7 +303,8 @@ function removeDir(location, next) {
                 }
             })
         }, function (err) {
-            if (err) return next(err)
+            if (err)
+                return next(err);
             fs.rmdir(location, function (err) {
                 return next(err)
             })
