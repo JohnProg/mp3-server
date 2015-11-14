36633810d33c63980bb75e4d4fbde2d22e2fe875
Norbert Songin <wezyrnorbert@gmail.com>
2015-11-14 13:46:57 +0100

    Saved table version of yt-mp3-downloader

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ path = require('path'),
 url = require('url'),
 events = require('events'),
 async = require('async'),
-YoutubeMp3Downloader = require('youtube-mp3-downloader'),
+YoutubeMp3Downloader = require('./youtube-mp3-downloader/index'),
 ytdl = require('ytdl-core');
 
 const ytApiKey = "AIzaSyBhtvRxAa5jePf6x0BRQCK-BBZ5-mhGwss";
